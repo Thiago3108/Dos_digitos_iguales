a7bc7adc05ea8200ed12c420226c66c3b3c5b540
Thiago3108 <santiago2234508@uis.edu.co>
2023-03-04 10:58:01 -0500

    se actializo el codigo para negativos

--- programa.py
+++ programa.py
@@ -14,14 +14,17 @@ if (n>0):
     ud = (n % 10)
     p = (n % 100)//10
     if (ud == p):
-       print("los dos ultimos digitos son iguales ")
+       rta = "los dos ultimos digitos son iguales "
     else:
-     print ("los dos ultimos digitos no son iguales")
+     rta = "los dos ultimos digitos no son iguales"
 else:
     udN = -(n % 10)
     pN = -(n % 100)//10
     if (udN == pN ):
-       print("los dos ultimos digitos son iguales ")
+       rta = "los dos ultimos digitos son iguales "
     else:
-       print ("los dos ultimos digitos no son iguales")
+       rta = "los dos ultimos digitos no son iguales"
+
+# output
+print(str(rta))
     
